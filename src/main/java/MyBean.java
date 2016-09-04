57df0e7f13dee0cec5ed4dafb03ef6553f914057
astashyshyn <astash@softserveinc.com>
2016-09-04 11:32:45 +0300

    Initial commit

---
+++ src/main/java/MyBean.java
@@ -0,0 +1,2 @@
+public class MyBean {
+}
---
+++ src/main/java/MyUtil.java
@@ -0,0 +1,6 @@
+
+public class MyUtil {
+    public static void validateSomething(){};
+    public static void loggingSomething(){};
+    public static void doSomeLongUtilWork(){};
+}
