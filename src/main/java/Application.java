9e49fc0af1e0193b40cb20368303560fb6429621
Andrii Stashyshyn <astash@softserveinc.com>
2016-09-07 23:06:14 +0300

    Work on 4 branch

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,9 @@ public class Application {
 
     public static void doSomething() {
         System.out.println("Hello world");
+        System.out.println("Starting working");
         MyUtil.doSomeLongUtilWork();
         System.out.println("Hello Jonny");
+        System.out.println("Hello Mike");
     }
 }
