2cf5bae74b4c5fdd4e44cd36b24d80581ae3f2a9
Andrii Stashyshyn <astash@softserveinc.com>
2016-09-09 17:32:53 +0300

    main work

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,11 @@
 public class Application {
     public static void main(String[] args) {
         doSomeStuff();
+        doSomeStuff2();
     }
 
     static void doSomeStuff(){}
+    static void doSomeStuff2(){
+        System.out.print("Hello");
+    }
 }
